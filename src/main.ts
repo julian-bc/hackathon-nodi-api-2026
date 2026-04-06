bac8e20b2068388232de92a7fe9c20c902c20651
Liang Camilo <liangcamilo2006@gmail.com>
2026-04-06 14:25:56 -0500

    feat: admin user added when booting application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app/app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ConfigService } from '@nestjs/config';
 import cookieParser from 'cookie-parser';
+import { Model } from 'mongoose';
+import { User } from './user/schema/user.schema';
+import { getModelToken } from '@nestjs/mongoose';
+import { DocumentTypes, UserRoles } from './user/types/user.types';
+import { HashService } from './common/hash/hash.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +16,12 @@ async function bootstrap() {
 
   const port = Number(configService.get<string>('PORT'));
   const frontendUrl = configService.get<string>('FRONTEND_URL');
+  const adminPassword = configService.get<string>('ADMIN_PASSWORD');
+  const adminEmail = configService.get<string>('ADMIN_EMAIL');
+
+  if (!adminPassword || !adminEmail) {
+    throw new Error('ADMIN_EMAIL y ADMIN_PASSWORD son obligatorios');
+  }
 
   app.enableCors({
     origin: frontendUrl ?? 'http://localhost:5173',
@@ -18,6 +29,35 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept',
   });
   app.use(cookieParser());
+
+  const userModel = app.get<Model<User>>(getModelToken(User.name));
+  const hashService = app.get(HashService);
+
+  await userModel.findOneAndUpdate(
+    { email: adminEmail },
+    {
+      $setOnInsert: {
+        name: 'ADMIN',
+        age: 129,
+        documentType: DocumentTypes.CC,
+        documentNumber: 999999999,
+        email: adminEmail,
+        password: await hashService.hashPassword(adminPassword),
+        phone: '3000000000',
+        role: UserRoles.administrador,
+        isEmailVerified: true,
+        pendingEmail: null,
+        emailChangeVerification: null,
+        forgotPasswordVerification: null,
+        registrationVerification: null,
+      },
+    },
+    {
+      upsert: true,
+      new: true,
+    },
+  );
+
   await app.listen(port ?? 3000);
 }
 bootstrap();
